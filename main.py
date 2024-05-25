267d4e011ea812fce68a42325f6338ff2963585a
wivest <120594169+wivest@users.noreply.github.com>
2024-05-25 16:38:23 +0200

    feat: enable KernelProfiler

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from application import Application
 from parameters import *
 
 
-ti.init(arch=ARCH, kernel_profiler=True)
-app = Application(COLS, ROWS)
-app.run()
+if __name__ == "__main__":
+	ti.init(arch=ARCH, kernel_profiler=True)
+	app = Application(COLS, ROWS)
+	app.run()
+	
+	if PROFILING:
+		ti.sync()
+		ti.profiler.print_kernel_profiler_info()
--- parameters.py
+++ parameters.py
@@ -5,6 +5,7 @@ from aliases import *
 COLS = 1024
 ROWS = 512
 ARCH = ti.gpu
+PROFILING = False
 
 ALIVE = Color(255, 255, 255)
 DEAD = Color(0, 0, 0)
