0caa4d1a446bb169a44bc7da258fcc8b0af9571d
wivest <120594169+wivest@users.noreply.github.com>
2024-08-03 03:22:38 +0200

    feat: get cursor coordinates

--- source/application.py
+++ source/application.py
@@ -42,3 +42,15 @@ class Application:
         for event in self.window.get_events(ti.ui.RELEASE):
             if event.key == ti.ui.LMB:
                 self.drawing = False
+
+    
+    def get_cursor_coordinates(self) -> tuple[int, int]:
+        clip = lambda value : max(0, min(value, 1))
+
+        cursor_pos = self.window.get_cursor_pos()
+        cursor_pos = (clip(cursor_pos[0]), clip(cursor_pos[1]))
+        window_size = self.window.get_window_shape()
+
+        x = int(cursor_pos[0] * window_size[0])
+        y = int(cursor_pos[1] * window_size[1])
+        return (x, y)
